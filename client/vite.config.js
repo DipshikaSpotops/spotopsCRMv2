fd727504af65cd1a15f1d834294280d5bbf3f857
Dipshika01 <dipshikapradhan2201@gmail.com>
2025-11-25 21:06:33 +0530

    gate bundle visualizer plugin behind ANALYZE_BUNDLE

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,50 +1,58 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 import { fileURLToPath, URL } from "node:url";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({
-      filename: "bundle-report.html",
-      open: false,
-      gzipSize: true,
-      brotliSize: true,
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-      "@spotops/shared": fileURLToPath(new URL("../shared", import.meta.url)),
-    },
-  },
+const shouldAnalyzeBundle = process.env.ANALYZE_BUNDLE === "true";
 
-  //moment-timezone from shared gets pre-bundled cleanly
-  optimizeDeps: {
-    include: ["moment-timezone"],
-  },
-  server: {
-    proxy: {
-      "/api": {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(async () => {
+  const plugins = [react()];
+
+  if (shouldAnalyzeBundle) {
+    const { visualizer } = await import("rollup-plugin-visualizer");
+    plugins.push(
+      visualizer({
+        filename: "bundle-report.html",
+        open: false,
+        gzipSize: true,
+        brotliSize: true,
+      })
+    );
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        "@spotops/shared": fileURLToPath(new URL("../shared", import.meta.url)),
       },
     },
-  },
-  build: {
-    target: "esnext",
-    sourcemap: false,
-    chunkSizeWarningLimit: 2500,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom"],
-          vendor: ["axios"], 
-          charts: ["chart.js"], 
+    // moment-timezone from shared gets pre-bundled cleanly
+    optimizeDeps: {
+      include: ["moment-timezone"],
+    },
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://127.0.0.1:5000",
+          changeOrigin: true,
+          secure: false,
         },
       },
     },
-  },
+    build: {
+      target: "esnext",
+      sourcemap: false,
+      chunkSizeWarningLimit: 2500,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react", "react-dom"],
+            vendor: ["axios"],
+            charts: ["chart.js"],
+          },
+        },
+      },
+    },
+  };
 });
