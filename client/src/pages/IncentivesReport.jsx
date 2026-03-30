909a851b4ecf7b8dcd7b6b2b0a2e72a57d1f84d5
SHANKAR'S PC <dipsikha.spotopsdigital@gmail.com>
2026-03-30 21:41:03 +0530

    making incentives repr

--- client/src/pages/IncentivesReport.jsx
+++ client/src/pages/IncentivesReport.jsx
@@ -142,7 +142,7 @@ export default function IncentivesReport() {
                       <th className="px-2 py-2 border-r border-white/20">Sales Report</th>
                       <th className="px-2 py-2 border-r border-white/20">Actual GP</th>
                       <th className="px-2 py-2 border-r border-white/20">Est GP</th>
-                      <th className="px-2 py-2 border-r border-white/20">ancelled Orders</th>
+                      <th className="px-2 py-2 border-r border-white/20">Cancelled Orders</th>
                       <th className="px-2 py-2 border-r border-white/20">Refunded Orders</th>
                       <th className="px-2 py-2 border-r border-white/20">Disputes</th>
                       <th className="px-2 py-2">Individual Report</th>
